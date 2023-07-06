d64993d9287c91f6fd20e199f39e91070e475be4
Pravat-21 <pravatpatra1997@gmail.com>
2023-07-06 23:17:00 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str):
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name='store_sales_prediction',
+    version='0.0.1',
+    author='Pravat Patra',
+    author_email='pravatpatra1997@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
