e8fd4373eee3e88527fef5affc1d04f90fee8ea1
Pravat-21 <pravatpatra1997@gmail.com>
2023-07-30 20:01:23 +0530

    data transformation

---
+++ src/components/data_transformation.py
@@ -0,0 +1,124 @@
+import sys
+from dataclasses import dataclass
+
+import numpy as np 
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder,StandardScaler,OneHotEncoder
+from sklearn.preprocessing import FunctionTransformer,PowerTransformer
+
+from src.exception import CustomException
+from src.logger import logging
+import os
+from src.utils import save_object
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+    def get_data_transformation_object(self):
+        try:
+            logging.info('Data Transformation initiated')
+            # Define which columns should be ordinal-encoded and which should be scaled
+            cat_cols1=['Item_Type', 'Item_Identifier']
+            cat_cols2=['Item_Fat_Content', 'Outlet_Size', 'Outlet_Location_Type', 'Outlet_Type']
+            num_cols=['Item_Weight', 'Item_Visibility', 'Item_MRP', 'Outlet_Age']
+            
+            # Define the custom ranking for each ordinal variable
+            fat=['Low Fat','Regular']
+            out_size=['Small','not_mentioned','Medium','High']
+            out_loc=['Tier 3','Tier 2','Tier 1']
+            out_type=['Supermarket Type3','Supermarket Type2','Supermarket Type1','Grocery Store']
+            
+            logging.info('Pipeline Initiated')
+
+            ## Numerical Pipeline
+            num_pipe=Pipeline(steps=[
+                ('imputer',SimpleImputer(strategy='median')),
+                ('power',PowerTransformer())
+            ])
+            cat_pipe1=Pipeline(steps=[
+                ('imputer',SimpleImputer(strategy='most_frequent')),
+                ('encoder',OneHotEncoder(sparse=False,drop='first')),
+                ('scaler',StandardScaler())
+                
+            ])
+
+            cat_pipe2=Pipeline(steps=[
+                ('imputer',SimpleImputer(strategy='most_frequent')),
+                ('encoder',OrdinalEncoder(categories=[fat,out_size,out_loc,out_type])),
+                ('scaler',StandardScaler())
+            ])
+            preprocessor=ColumnTransformer(transformers=[
+                ('transformer1',num_pipe,num_cols),
+                ('transformer2',cat_pipe1,cat_cols1),
+                ('transformer3',cat_pipe2,cat_cols2),
+            ])
+
+            logging.info('Pipeline Completed')
+
+            return preprocessor
+
+            
+
+        except Exception as e:
+            logging.info("Error in Data Trnasformation")
+            raise CustomException(e,sys)
+        
+    def initaite_data_transformation(self,train_path,test_path):
+        try:
+            # Reading train and test data
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info('Read train and test data completed')
+            logging.info(f'Train Dataframe Head : \n{train_df.head().to_string()}')
+            logging.info(f'Test Dataframe Head  : \n{test_df.head().to_string()}')
+
+            logging.info('Obtaining preprocessing object')
+
+            preprocessing_obj = self.get_data_transformation_object()
+
+            target_column_name = 'Item_Outlet_Sales'
+            #drop_columns = [target_column_name,'id']
+
+            input_feature_train_df = train_df.drop(columns=target_column_name,axis=1)
+            target_feature_train_df=train_df[target_column_name]
+
+            input_feature_test_df=test_df.drop(columns=target_column_name,axis=1)
+            target_feature_test_df=test_df[target_column_name]
+            
+            ## Trnasformating using preprocessor obj
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+
+            logging.info("Applying preprocessing object on training and testing datasets.")
+            
+
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            save_object(
+
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+
+            )
+            logging.info('Preprocessor pickle file saved')
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+            
+        except Exception as e:
+            logging.info("Exception occured in the initiate_datatransformation")
+
+            raise CustomException(e,sys)
